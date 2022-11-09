a46618d603cb4b4b48b7ef2a5b47c1cd54101561
Lenovo <Lenovo@LAPTOP-PCA3THCH>
2022-11-09 23:31:29 +0100

    Add project files.

---
+++ OOP Homework №5/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PrototypeFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            IFigure figure = new Rectangle(10, 20);
+            IFigure clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            figure = new Circle(15);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+
+            figure = new Triangle(10, 5);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            Console.Read();
+        }
+    }
+    interface IFigure
+    {
+        IFigure Clone();
+
+ void GetInfo();
+    }
+    class Rectangle : IFigure
+    {
+        int width;
+        int height;
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public IFigure Clone()
+        {
+            return new Rectangle(this.width, this.height);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Прямокутник довжиною {0} и шириною {0}", height, width);
+        }
+    }
+    class Circle : IFigure
+    {
+        int radius;
+        public Circle(int r)
+        {
+            radius = r;
+        }
+        public IFigure Clone()
+        {
+            return new Circle(this.radius);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Круг радіусом {0}", radius);
+        }
+    }
+
+    class Triangle : IFigure
+    {
+        int Base, Height;
+        public Triangle(int Base, int Height)
+        {
+            this.Base = Base;
+            this.Height = Height;
+        }
+        public IFigure Clone()
+        {
+            return new Triangle(this.Base, this.Height);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Трикутник з основою {0} та висотою {0}", Base, Height);
+        }
+    }
+}
